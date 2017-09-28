e15f11ac30fcc3be25160c2ef8a599d9aa5d29cb
sourabh kumar <sourabh.kumar@reece.com.au>
2017-09-28 11:40:46 +1000

    koka stategy

---
+++ rock_paper_scissors/league/koka.py
@@ -0,0 +1,21 @@
+import random
+from rock_paper_scissors import ROCK, PAPER, SCISSORS, BasePlayer
+
+
+class Player(BasePlayer):
+
+    player_name = 'Koka'
+    author = 'Sourabh K'
+
+    def __init__(self):
+        self.history = []
+        self.current_choice = None
+
+    def play(self):
+        self.current_choice = random.choice(
+            [PAPER, ROCK, SCISSORS]
+        )
+        return self.current_choice
+
+    def result(self, their_play):
+        self.history.append((self.current_choice, their_play))
